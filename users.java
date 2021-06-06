dca99ed9bb4e686f40a5b4d1093d7d1fdfc46844
knraodb <knraodb@gmail.com>
2021-06-06 09:20:38 +0530

    fileupdated

--- users.java
+++ users.java
@@ -3,3 +3,5 @@ this file contains users' information
 add new line in file in master branch
 Updaitng file in development branch
 adding this line for git stash demo1
+adding this line for git stash demo2
+adding this line for git stash with comment
